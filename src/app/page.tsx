e88af25746730a251a4613a7764a1bd73fa41e4a
Sebastian <snrnieto1999@hotmail.com>
2023-07-07 18:25:09 -0500

    [ADD] response llm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,85 @@
-import Image from 'next/image'
+"use client";
+import useLLM from "usellm";
+import { useState } from "react";
 
-export default function Home() {
+export default function Demo() {
+  const llm = useLLM({ serviceUrl: "https://usellm.org/api/llm" });
+  const [result, setResult] = useState("");
+  const [inputText, setInputText] = useState("");
+  const maxLength = 150;
+
+  async function handleClick() {
+    if (result.length > 0) {
+      setResult("");
+      setInputText("");
+    } else {
+      try {
+        if (inputText.length > 0) {
+          await llm.chat({
+            messages: [
+              {
+                role: "system",
+                content:
+                  "Eres un interpretador de sueños latinoamericano, debes responder tu interpretacion de forma amable y sincera en menos de 150 caracteres",
+              },
+              { role: "user", content: inputText },
+            ],
+            stream: true,
+            onStream: ({ message }) => setResult(message.content),
+          });
+        } else {
+          alert("Escribe un mensaje");
+        }
+      } catch (error) {
+        console.error("Something went wrong!", error);
+      }
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex justify-center text-justify  px-3 sm:px-0">
+      <div className="max-w-xl flex flex-col gap-4 w-full">
+        <h1 className="text-center text-3xl uppercase">Dream Meaning</h1>
+        <p className="text-center text-xl my-2">Escribe tu sueño</p>
+        {result.length == 0 && (
+          <p className="font-sans text-sm">
+            Proporcione una descripción detallada y emocional de su sueño; esto
+            nos permitirá analizar las posibles causas y significados de su
+            sueño.
+            <br /> <br />
+            Por ejemplo, en lugar de decir 'soñé que estaba en la oficina',
+            escribe 'soñé que estaba en la oficina trabajando hasta tarde,
+            sintiéndome abrumado y estresado por todas las tareas que tenía que
+            realizar'.
+          </p>
+        )}
+        <div className="w-full relative">
+          <textarea
+            value={inputText}
+            maxLength={maxLength}
+            className="bg-white bg-opacity-10 rounded-3xl p-3 w-full h-[30vh]"
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Soñe con..."
+            disabled={result.length > 0}
+          />
+          <span className="absolute bottom-3 right-3">
+            {inputText.length}/{maxLength}
+          </span>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {result.length > 0 && (
+          <textarea
+            value={result}
+            style={{ whiteSpace: "pre-wrap" }}
+            className="bg-white bg-opacity-10  rounded-3xl p-3 w-full h-[30vh] text-white"
+            disabled
+          />
+        )}
+        <button
+          onClick={handleClick}
+          className="border border-white rounded-xl py-2"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {result.length > 0 ? "Analizar otro sueño" : "Analizar sueño"}
+        </button>
       </div>
     </main>
-  )
+  );
 }
