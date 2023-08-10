5c709500ac04770ac42768bb98e2d5f291b09c63
Sebastian <snrnieto1999@hotmail.com>
2023-08-10 15:22:57 -0500

    [ADD] intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,10 +2,12 @@
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 import { toast } from "react-hot-toast";
+import { useTranslations } from "next-intl";
 
 export default function Page() {
   const router = useRouter();
   const [email, setEmail] = useState("");
+  const t = useTranslations("Landing");
 
   function handleSubmitEmail(event: FormEvent<HTMLFormElement>): void {
     event.preventDefault();
@@ -45,12 +47,10 @@ export default function Page() {
         />
         <div>
           <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white text-center lg:text-start">
-            Unlock the Hidden Secrets of Your Dreams
+            {t("title")}
           </h2>
           <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-            Dream Meaning is an AI-powered dream analysis app. Uncover dream
-            meanings & symbolism. Get personalized insights from advanced AI.
-            Keep a dream journal & discover yourself through dreams.
+            {t("description")}
           </p>
           <form
             className="flex flex-col md:flex-row items-center justify-between gap-6 "
@@ -76,14 +76,14 @@ export default function Page() {
                 onChange={(e) => setEmail(e.target.value)}
                 required
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Enter your email"
+                placeholder={t("placehodlerEmail")}
               />
             </div>
             <button
               className="whitespace-nowrap inline-flex items-center justify-center px-5 py-2  w-full md:w-auto text-base font-medium text-center text-white rounded-lg  bg-blue-600 hover:bg-blue-800"
               type="submit"
             >
-              Try for free
+              {t("button")}
             </button>
           </form>
         </div>
